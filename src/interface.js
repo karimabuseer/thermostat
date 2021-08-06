da79b873aec326da80164a5c6eb52ddc27c00891
karimabuseer <karimabuseer@gmail.com>
2021-08-06 15:03:00 +0100

    Working frontpage

--- src/interface.js
+++ src/interface.js
@@ -1,3 +1,32 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-  console.log('Hello there!')
+  const tempUp = document.getElementById("tempup");
+  const tempDown = document.getElementById("tempdown");
+  const powerSaveOn = document.getElementById("pson")
+  const powerSaveOff = document.getElementById("psoff")
+
+
+  const thermostat = new Thermostat();
+
+  tempUp.addEventListener("click",function() {
+    console.log(thermostat.degrees)
+    thermostat.up()
+    document.getElementById("temperature").innerHTML = thermostat.degrees;
+  });
+  tempDown.addEventListener("click",function() {
+    console.log(thermostat.degrees)
+    thermostat.down()
+    document.getElementById("temperature").innerHTML = thermostat.degrees;
+  });
+  powerSaveOn.addEventListener("click",function() {
+    thermostat.powerSaveOn()
+    document.getElementById("powersaving").innerHTML = thermostat.powerSaveMode;
+  });
+
+  powerSaveOff.addEventListener("click",function() {
+    thermostat.powerSaveOff()
+    document.getElementById("powersaving").innerHTML = thermostat.powerSaveMode;
+  });
+
+  document.getElementById("temperature").innerHTML = thermostat.degrees;
+  document.getElementById("powersaving").innerHTML = thermostat.powerSaveMode;
 });
